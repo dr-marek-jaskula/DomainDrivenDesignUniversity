8b5a22f490d27865cebfe33f0e2b9e5c0eb23c96
Marek Jaskuła <Marek.Jaskula1@gmail.com>
2022-11-13 21:50:47 +0100

    Comments

--- src/Shop.Domain/Errors/HttpErrors.cs
+++ src/Shop.Domain/Errors/HttpErrors.cs
@@ -2,6 +2,12 @@
 
 public static class HttpErrors
 {
+    /// <summary>
+    /// Create an Error based on the entity type name and the id that was not found
+    /// </summary>
+    /// <param name="name">name of the entity type. Use "nameof(TValue)" syntax</param>
+    /// <param name="id">id of the entity that was not found</param>
+    /// <returns>NotFound error</returns>
     public static Error NotFound(string name, Guid id)
     {
         return new Error($"{name}.NotFound", $"{name} with Id: '{id}' was not found");
--- src/Shop.Domain/Primitives/IAuditableEntity.cs
+++ src/Shop.Domain/Primitives/IAuditableEntity.cs
@@ -2,7 +2,7 @@
 
 //We use the Auditable pattern to set the "CreatedOn" and "UpdatedOn" properties when saving changes.
 //For this to happen we can use Interceptor pattern or override the SaveChanges (and SaveChangesAsync) methods
-//The preferred way: use the "Interceptor" pattern
+//The preferred way: override SaveChanges
 public interface IAuditableEntity
 {
     DateTimeOffset CreatedOn { get; set; }
