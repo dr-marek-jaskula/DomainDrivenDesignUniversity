a1de71900455973784397a5d4573170e2262fd4c
Marek <Marek@DESKTOP-8BJGCQ1>
2023-04-28 11:16:57 +0200

    typo

--- src/Shopway.Persistance/Utilities/SpecificationUtilities.cs
+++ src/Shopway.Persistance/Utilities/SpecificationUtilities.cs
@@ -7,12 +7,11 @@ namespace Shopway.Persistence.Utilities;
 internal static class SpecificationUtilities
 {
     /// <summary>
-    /// Add pagination from specification
+    /// Cast specification to mapping specification
     /// </summary>
     /// <typeparam name="TEntity">Entity type</typeparam>
     /// <typeparam name="TEntityId">EntityId type</typeparam>
     /// <typeparam name="TResponse">Response type</typeparam>
-    /// <param name="queryable">_dbContext.Set<TResponse>()</param>
     /// <param name="specification">Input specification</param>
     /// <returns>Queryable</returns>
     internal static SpecificationWithMappingBase<TEntity, TEntityId, TResponse> AsMappingSpecification<TEntity, TEntityId, TResponse>(this SpecificationBase<TEntity, TEntityId> specification)
