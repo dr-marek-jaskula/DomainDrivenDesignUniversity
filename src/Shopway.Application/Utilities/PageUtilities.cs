3759dbc6ac185eadc3eb3e678d841d745fe56e03
Marek <Marek@DESKTOP-8BJGCQ1>
2023-04-28 10:15:53 +0200

    simplify

--- src/Shopway.Application/Utilities/PageUtilities.cs
+++ src/Shopway.Application/Utilities/PageUtilities.cs
@@ -1,36 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Shopway.Application.Abstractions;
+﻿using Shopway.Application.Abstractions;
 using Shopway.Application.CQRS;
-using Shopway.Domain.Abstractions;
-using Shopway.Domain.Utilities;
-using System.Linq.Expressions;
 
 namespace Shopway.Application.Utilities;
 
 public static class PageUtilities
 {
-    public static async Task<PageResponse<TResponse>> ToPageResponse<TEntity, TResponse>
-    (
-        this IQueryable<TEntity> queryable,
-        int pageSize,
-        int pageNumber,
-        Expression<Func<TEntity, TResponse>> fromEntityToResponseMapper,
-        CancellationToken cancellationToken
-    )
-        where TEntity : class, IEntity
-        where TResponse : class, IResponse
-    {
-        var response = await queryable
-            .Page(pageSize, pageNumber)
-            .Select(fromEntityToResponseMapper)
-            .ToListAsync(cancellationToken);
-
-        var totalCount = await queryable
-            .CountAsync(cancellationToken);
-
-        return new PageResponse<TResponse>(response, totalCount, pageSize, pageNumber);
-    }
-
     public static PageResponse<TResponse> ToPageResponse<TResponse>(this (IList<TResponse> Responses, int Count) response, Page page)
         where TResponse : class, IResponse
     {
