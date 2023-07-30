c15c171f15b58c3f81c353dfb3208699aebdb00a
Marek <Marek@DESKTOP-8BJGCQ1>
2023-07-30 16:42:10 +0200

    add QuartzOptionsSetup

---
+++ src/Shopway.Infrastructure/Options/QuartzOptionsSetup.cs
@@ -0,0 +1,23 @@
+﻿using Quartz;
+using Microsoft.Extensions.Options;
+using Shopway.Infrastructure.BackgroundJobs;
+
+namespace Shopway.Infrastructure.Options;
+
+public sealed class QuartzOptionsSetup : IConfigureOptions<QuartzOptions>
+{
+    public void Configure(QuartzOptions options)
+    {
+        var jobKey = JobKey.Create(nameof(ProcessOutboxMessagesJob));
+
+        options
+            .AddJob<ProcessOutboxMessagesJob>(jobBuilder => jobBuilder.WithIdentity(jobKey))
+            .AddTrigger(trigger =>
+                trigger
+                    .ForJob(jobKey)
+                    .WithSimpleSchedule(schedule =>
+                        schedule
+                            .WithIntervalInSeconds(10)
+                            .RepeatForever()));
+    }
+}
