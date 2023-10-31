1bf0b1b0fc72b9ed20d4c8c20817e7d033538e6a
dr Marek Jaskuła <76631630+dr-marek-jaskula@users.noreply.github.com>
2023-10-31 16:29:15 +0100

    add exception asserting example (#37)
    Co-authored-by: Marek <Marek@DESKTOP-8BJGCQ1>

---
+++ test/Shopway.Tests.Unit/LayerTests/Domain/Common/ErrorTests.cs
@@ -0,0 +1,25 @@
+﻿using Shopway.Domain.Errors;
+using Shopway.Tests.Unit.Constants;
+
+namespace Shopway.Tests.Unit.LayerTests.Domain.Common;
+
+[Trait(nameof(UnitTest), UnitTest.Domain)]
+public sealed class ErrorTests
+{
+    [Fact]
+    public void ThrowIfErrorNone_ShouldThrowAnException_WhenErrorIsNone()
+    {
+        //Arrange
+        var error = Error.None;
+
+        //Assert
+        FluentActions
+            .Invoking(error.ThrowIfErrorNone)
+            .Should()
+            .ThrowExactly<InvalidOperationException>()
+            .Which
+            .Message
+            .Should()
+            .Be("Provided error is Error.None");
+    }
+}
---
+++ test/Shopway.Tests.Unit/LayerTests/Domain/Common/ResultTests.cs
@@ -0,0 +1,27 @@
+﻿using Shopway.Domain.Errors;
+using Shopway.Domain.Results;
+using Shopway.Tests.Unit.Constants;
+
+namespace Shopway.Tests.Unit.LayerTests.Domain.Common;
+
+[Trait(nameof(UnitTest), UnitTest.Domain)]
+public sealed class ResultTests
+{
+    [Fact]
+    public void GettingValueFromResult_ShouldThrowAnException_WhenResultIsFailureResult()
+    {
+        //Arrange
+        var result = Result.Failure<string>(Error.ConditionNotSatisfied);
+        string GetValueFromFailureResult() => result.Value;
+
+        //Assert
+        FluentActions
+            .Invoking(GetValueFromFailureResult)
+            .Should()
+            .ThrowExactly<InvalidOperationException>()
+            .Which
+            .Message
+            .Should()
+            .Be("The value of a failure result can not be accessed. Type 'System.String'.");
+    }
+}
