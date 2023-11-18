12daa3d85c45d10cc678e12b4e1de4f1086617e8
Marek <Marek@DESKTOP-8BJGCQ1>
2023-11-18 14:53:42 +0100

    add primary constructor for requirements

--- src/Shopway.Presentation/Authentication/PermissionAuthentication/Requirements/PermissionRequirement.cs
+++ src/Shopway.Presentation/Authentication/PermissionAuthentication/Requirements/PermissionRequirement.cs
@@ -2,12 +2,7 @@
 
 namespace Shopway.Presentation.Authentication.PermissionAuthentication.Requirements;
 
-public sealed class PermissionRequirement : IAuthorizationRequirement
+public sealed class PermissionRequirement(string permission) : IAuthorizationRequirement
 {
-    public PermissionRequirement(string permission)
-    {
-        Permission = permission;
-    }
-
-    public string Permission { get; }
+    public string Permission { get; } = permission;
 }
