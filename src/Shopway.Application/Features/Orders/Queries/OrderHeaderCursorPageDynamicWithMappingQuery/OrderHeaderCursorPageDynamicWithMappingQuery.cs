5d3a3ae1ea47af7f89328e4f3bf6d3f3fb6025dd
dr Marek Jaskuła <76631630+dr-marek-jaskula@users.noreply.github.com>
2024-03-13 17:47:56 +0100

    add cursor page query for proxy service (#97)
    Co-authored-by: Marek <Marek@DESKTOP-8BJGCQ1>

---
+++ src/Shopway.Application/Features/Orders/Queries/OrderHeaderCursorPageDynamicWithMappingQuery/OrderHeaderCursorPageDynamicWithMappingQuery.cs
@@ -0,0 +1,14 @@
+﻿using Shopway.Application.Abstractions.CQRS;
+using Shopway.Domain.Common.DataProcessing;
+using Shopway.Domain.Orders.DataProcessing.Filtering;
+using Shopway.Domain.Orders.DataProcessing.Mapping;
+using Shopway.Domain.Orders.DataProcessing.Sorting;
+
+namespace Shopway.Application.Features.Orders.Queries.DynamicCursorOrderHeaderWithMappingQuery;
+
+public sealed record OrderHeaderCursorPageDynamicWithMappingQuery(CursorPage Page) : ICursorPageQuery<DataTransferObjectResponse, OrderHeaderDynamicFilter, OrderHeaderDynamicSortBy, OrderHeaderDynamicMapping, CursorPage>
+{
+    public OrderHeaderDynamicFilter? Filter { get; init; }
+    public OrderHeaderDynamicSortBy? SortBy { get; init; }
+    public OrderHeaderDynamicMapping? Mapping { get; init; }
+}
---
+++ src/Shopway.Application/Features/Orders/Queries/OrderHeaderCursorPageDynamicWithMappingQuery/OrderHeaderCursorPageDynamicWithMappingQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using Shopway.Application.Abstractions.CQRS;
+using Shopway.Domain.Common.DataProcessing;
+using Shopway.Domain.Common.Results;
+using Shopway.Domain.Orders.DataProcessing.Filtering;
+using Shopway.Domain.Orders.DataProcessing.Mapping;
+using Shopway.Domain.Orders.DataProcessing.Sorting;
+using Shopway.Domain.Orders;
+using Shopway.Application.Utilities;
+
+namespace Shopway.Application.Features.Orders.Queries.DynamicCursorOrderHeaderWithMappingQuery;
+
+internal sealed class OrderHeaderCursorPageDynamicWithMappingQueryHandler(IOrderHeaderRepository orderHeaderRepository)
+    : ICursorPageQueryHandler<OrderHeaderCursorPageDynamicWithMappingQuery, DataTransferObjectResponse, OrderHeaderDynamicFilter, OrderHeaderDynamicSortBy, OrderHeaderDynamicMapping, CursorPage>
+{
+    private readonly IOrderHeaderRepository _orderHeaderRepository = orderHeaderRepository;
+
+    public async Task<IResult<CursorPageResponse<DataTransferObjectResponse>>> Handle(OrderHeaderCursorPageDynamicWithMappingQuery pageQuery, CancellationToken cancellationToken)
+    {
+        var page = await _orderHeaderRepository
+            .PageAsync(pageQuery.Page, cancellationToken, filter: pageQuery.Filter, sort: pageQuery.SortBy, mapping: pageQuery.Mapping);
+
+        return page
+            .ToPageResponse(pageQuery.Page)
+            .ToResult();
+    }
+}
