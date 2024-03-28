c6ad5315d1e14882d237471b9019add329ef3016
Marek <Marek@DESKTOP-8BJGCQ1>
2024-03-28 21:10:29 +0100

    fix

--- src/Shopway.Persistence/Configurations/UserConfiguration.cs
+++ src/Shopway.Persistence/Configurations/UserConfiguration.cs
@@ -58,7 +58,7 @@ internal sealed class UserEntityTypeConfiguration : IEntityTypeConfiguration<Use
         builder.Property(u => u.TwoFactorToptSecret)
             .HasConversion<TwoFactorToptSecretConverter, TwoFactorToptSecretComparer>()
             .HasColumnName(nameof(TwoFactorToptSecret))
-            .HasColumnType(ColumnType.NChar(TwoFactorToptSecret.BytesLong))
+            .HasColumnType(ColumnType.VarChar(TwoFactorToptSecret.BytesLong))
             .IsRequired(false);
 
         builder.Property(entity => entity.TwoFactorTokenCreatedOn)
