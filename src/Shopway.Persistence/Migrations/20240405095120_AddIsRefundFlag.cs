563e9d6546a9d5fb2b4b2d36c1ebde2ff320ae7c
Marek <Marek@DESKTOP-8BJGCQ1>
2024-04-05 11:52:18 +0200

    migration

---
+++ src/Shopway.Persistence/Migrations/20240405095120_AddIsRefundFlag.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shopway.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsRefundFlag : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRefunded",
+                schema: "Shopway",
+                table: "Payment",
+                type: "Bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsRefunded",
+                schema: "Shopway",
+                table: "Payment");
+        }
+    }
+}
