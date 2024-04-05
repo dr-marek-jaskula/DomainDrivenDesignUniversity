414d2b3687c1995a5b1da1c23aeeae85c0332e29
Marek <Marek@DESKTOP-8BJGCQ1>
2024-04-05 11:06:38 +0200

    add comment

--- src/Shopway.Infrastructure/Payments/DummyGatewayTypes/SessionService.cs
+++ src/Shopway.Infrastructure/Payments/DummyGatewayTypes/SessionService.cs
@@ -2,6 +2,10 @@
 
 public class SessionService
 {
+    /// <summary>
+    /// This send a request to the Payment Gateway to create a session for a customer. This session will be available on a given url.
+    /// For demo purposes, there is no need to create a session dummy Payment Gateway side.
+    /// </summary>
     internal async Task<Session> CreateAsync(SessionCreateOptions options)
     {
         return new Session()
