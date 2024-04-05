bd99f03efa7a5dc0945089156c525ade47031ba3
Marek <Marek@DESKTOP-8BJGCQ1>
2024-04-05 11:07:27 +0200

    use TaskFromResult

--- src/Shopway.Infrastructure/Payments/DummyGatewayTypes/SessionService.cs
+++ src/Shopway.Infrastructure/Payments/DummyGatewayTypes/SessionService.cs
@@ -6,13 +6,13 @@ public class SessionService
     /// This send a request to the Payment Gateway to create a session for a customer. This session will be available on a given url.
     /// For demo purposes, there is no need to create a session dummy Payment Gateway side.
     /// </summary>
-    internal async Task<Session> CreateAsync(SessionCreateOptions options)
+    internal Task<Session> CreateAsync(SessionCreateOptions options)
     {
-        return new Session()
+        return Task.FromResult(new Session()
         {
             ClientSecret = Guid.NewGuid().ToString(),
             Id = Guid.NewGuid().ToString(),
             Url = "http://localhost:61045/redirect-to-payment-session"
-        };
+        });
     }
 }
