0278345b9442e288b18d203d727aa4e741911789
Marek <Marek@DESKTOP-8BJGCQ1>
2024-04-06 12:40:32 +0200

    add wire mock

---
+++ test/Shopway.Tests.Integration.Container/WireMockUtilities/ResponseProviders/ResponseEntry.cs
@@ -0,0 +1,8 @@
+﻿using System.Net;
+
+namespace Shopway.Tests.Integration.Container.WireMockUtilities.ResponseProviders;
+
+public sealed record ResponseEntry(HttpStatusCode HttpStatusCode, object? Body = default)
+{
+    public bool HasBody => Body is not null;
+}
