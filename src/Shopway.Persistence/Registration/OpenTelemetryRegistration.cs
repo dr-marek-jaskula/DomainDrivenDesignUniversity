65b579339d7111c2272658268bfcc7e83045a8c2
hhko <hyungho.ko@gmail.com>
2024-05-23 07:06:33 +0900

    refactor: fix names

--- src/Shopway.Persistence/Registration/OpenTelemetryRegistration.cs
+++ src/Shopway.Persistence/Registration/OpenTelemetryRegistration.cs
@@ -35,6 +35,9 @@ internal static class OpenTelemetryRegistration
                 options.AddOtlpExporter(options => ConfigureOtlpCollectorExporter(options, openTelemetryOptions.OtlpCollectorHost));
             }
 
+            options.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(
+                serviceName: nameof(Shopway)));
+
             options.AddProcessor(new ActivityEventLogProcessor());
         });
 
