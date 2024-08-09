d8db1958e28bec2287f29fba0939a089ca6ce711
Marek <Marek@DESKTOP-8BJGCQ1>
2024-08-09 10:33:24 +0200

    refactor registration

--- src/Shopway.Presentation/Registration/MinimalApiRegistration.cs
+++ src/Shopway.Presentation/Registration/MinimalApiRegistration.cs
@@ -9,43 +9,62 @@ public static class MinimalApiRegistration
 {
     internal static IApplicationBuilder UseMinimalApiEndpoints(this WebApplication app)
     {
-        var endpointTypes = GetEndpointTypes();
         var baseGroup = BaseEndpointGroup.RegisterEndpointGroup(app);
 
-        foreach (var endpointType in endpointTypes)
+        var endpointInfos = AssemblyReference.Assembly
+            .GetEndpointTypes()
+            .Select(type => (type.GetEndpointRegisterMethod(), type.GetEndpointGroupType()));
+
+        foreach ((MethodInfo endpointRegisterMethod, Type? endpointGroupType) in endpointInfos)
         {
-            var endpointGroup = GetEndpointGroupType(endpointType);
-            var endpointRegisterMethod = endpointType.GetStaticMethod(nameof(IEndpoint.RegisterEndpoint));
-
-            if (endpointGroup is null)
-            {
-                endpointRegisterMethod.Invoke(null, [baseGroup]);
-                continue;
-            }
-
-            var endpointGroupRegisterMethod = endpointGroup.GetStaticMethod(nameof(IEndpointGroup.RegisterEndpointGroup));
-            var routeGroupBuilder = endpointGroupRegisterMethod.Invoke(null, [baseGroup]);
-            endpointRegisterMethod.Invoke(null, [routeGroupBuilder]);
+            var endpointGroupRegisterMethod = endpointGroupType.GetEndpointGroupRegisterMethodOrDefault();
+            endpointRegisterMethod.RegisterEndpoint(baseGroup, endpointGroupRegisterMethod);
         }
 
         return app;
     }
 
-    private static TypeInfo[] GetEndpointTypes()
+    private static TypeInfo[] GetEndpointTypes(this Assembly assembly)
     {
-        return AssemblyReference.Assembly
+        return assembly
             .DefinedTypes
             .Where(type => type is { IsAbstract: false, IsInterface: false } && type.IsAssignableTo(typeof(IEndpoint)))
             .ToArray();
     }
 
-    private static Type? GetEndpointGroupType(TypeInfo endpoint)
+    private static Type? GetEndpointGroupType(this TypeInfo endpoint)
     {
         return endpoint
             .GetInterfaces()
-            .First(x => x.IsGenericType && x.Name.Contains(nameof(IEndpoint)))
+            .Single(x => x.IsGenericType && x.Name.Contains(nameof(IEndpoint)))
             .GetGenericArguments()
-            .FirstOrDefault(x => x.IsAssignableTo(typeof(IEndpointGroup)));
+            .SingleOrDefault(x => x.IsAssignableTo(typeof(IEndpointGroup)));
+    }
+
+    private static void RegisterEndpoint(this MethodInfo registerEndpointMethod, object group, MethodInfo? registerEndpointGroupMethod = null)
+    {
+        if (registerEndpointGroupMethod is null)
+        {
+            registerEndpointMethod.Invoke(null, [group]);
+        }
+
+        var routeGroupBuilder = registerEndpointGroupMethod!.Invoke(null, [group]);
+        registerEndpointMethod.Invoke(null, [routeGroupBuilder]);
+    }
+
+    private static MethodInfo? GetEndpointGroupRegisterMethodOrDefault(this Type? endpointGroupType)
+    {
+        if (endpointGroupType is null)
+        {
+            return null;
+        }
+
+        return endpointGroupType.GetStaticMethod(nameof(IEndpointGroup.RegisterEndpointGroup));
+    }
+
+    private static MethodInfo GetEndpointRegisterMethod(this TypeInfo endpointType)
+    {
+        return endpointType.GetStaticMethod(nameof(IEndpoint.RegisterEndpoint));
     }
 
     private static MethodInfo GetStaticMethod(this Type type, string name)
