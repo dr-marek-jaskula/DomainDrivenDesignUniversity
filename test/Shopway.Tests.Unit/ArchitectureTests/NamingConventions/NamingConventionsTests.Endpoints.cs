80b420791765d723b91058e3be86a270c4b38d86
Marek <Marek@DESKTOP-8BJGCQ1>
2024-08-09 10:55:45 +0200

    add architecture naming convention test

--- test/Shopway.Tests.Unit/ArchitectureTests/NamingConventions/NamingConventionsTests.Endpoints.cs
+++ test/Shopway.Tests.Unit/ArchitectureTests/NamingConventions/NamingConventionsTests.Endpoints.cs
@@ -9,7 +9,7 @@ namespace Shopway.Tests.Unit.ArchitectureTests.NamingConventions;
 public partial class NamingConventionsTests
 {
     [Fact]
-    public void EndpointsNames_ShouldEndWithEndpoint()
+    public void FastEndpointsNames_ShouldEndWithEndpoint()
     {
         //Arrange
         var assembly = Shopway.Presentation.AssemblyReference.Assembly;
@@ -21,6 +21,27 @@ public partial class NamingConventionsTests
             .AreClasses()
             .And()
             .Inherit(typeof(BaseEndpoint))
+            .Should()
+            .HaveNameEndingWith(Endpoint)
+            .GetResult();
+
+        //Assert
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void MinimalEndpointsNames_ShouldEndWithEndpoint()
+    {
+        //Arrange
+        var assembly = Shopway.Presentation.AssemblyReference.Assembly;
+
+        //Act
+        var result = Types
+            .InAssembly(assembly)
+            .That()
+            .AreClasses()
+            .And()
+            .ImplementInterface(typeof(Shopway.Presentation.Abstractions.IEndpoint))
             .Should()
             .HaveNameEndingWith(Endpoint)
             .GetResult();
